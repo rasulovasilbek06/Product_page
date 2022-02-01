12ae486722424c73a1ad15c4900caf52d0595a81
Asilbekrasul <96375724+Asilbekrasul@users.noreply.github.com>
2022-02-01 15:47:05 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// document.getElementById('ch1').onclick = changeColor;   
+
+// function changeColor() {
+//     document.body.style.backgroundColor = "blue";
+//     return true;
+// }   
+
+// document.getElementById("ch1").style.background='white';
+// document.getElementById("ch2").style.background='white';
+// document.getElementById("ch3").style.background='white';
+
+
